772154abe469ec70a08abc97a4ba40de6da87030
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2025-10-18 13:22:50 +0100

    submit assignment

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://staging.trymima.com",
+    defaultCommandTimeout: 10000,
+    viewportHeight: 960,
+    viewportWidth: 1530,
+    setupNodeEvents(on, config) {
+      //implement node event listeners here
+    }
+  },
+});
